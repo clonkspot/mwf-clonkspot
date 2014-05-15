0d4659f4fe3c7217724e1f57ebab80ab78283e51
Luchs <Luchs@euirc.eu>
2014-05-15 21:48:44 +0200

    Add translations for two style snippets

--- data/MwfEnglishLocal.pm
+++ data/MwfEnglishLocal.pm
@@ -1,3 +1,8 @@
 
 $lng->{errBlockEmlT} .= " <a href='topic_show.pl?tid=139' target='_blank'>More information...</a>";
+
+# Style Snippets
+$lng->{sty1NoLogo} = "Hide Clonkspot logo";
+$lng->{sty2NoNav} = "Hide Clonkspot navigation";
+
 1;
--- data/MwfGermanLocal.pm
+++ data/MwfGermanLocal.pm
@@ -1,3 +1,8 @@
 
 $lng->{errBlockEmlT} .= " <a href='topic_show.pl?tid=138' target='_blank'>Mehr Informationen...</a>";
+
+# Style Snippets
+$lng->{sty1NoLogo} = "Clonkspot-Logo verstecken";
+$lng->{sty2NoNav} = "Clonkspot-Navigation verstecken";
+
 1;
