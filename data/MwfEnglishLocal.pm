85f0982fbc4505c754f698ee2223d6258383d497
Luchs <Luchs@euirc.eu>
2014-05-15 21:33:31 +0200

    Add local strings files

---
+++ data/MwfEnglishLocal.pm
@@ -0,0 +1,3 @@
+
+$lng->{errBlockEmlT} .= " <a href='topic_show.pl?tid=139' target='_blank'>More information...</a>";
+1;
---
+++ data/MwfGermanLocal.pm
@@ -0,0 +1,3 @@
+
+$lng->{errBlockEmlT} .= " <a href='topic_show.pl?tid=138' target='_blank'>Mehr Informationen...</a>";
+1;
